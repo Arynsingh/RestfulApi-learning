ce7bb46361b95d854c9924c65389f6fd9704bd59
Aryan Singh <aryanparmar9801@gmail.com>
2026-08-06 00:32:41 +0530

    Error standarized response

--- src/common/config/utils/api-error.js
+++ src/common/config/utils/api-error.js
@@ -1,7 +1,17 @@
-class ApiError extends Error{
-  constructor(statusCode,message){
-    super(message) // super calls parent constructor of parent class
-    this.statusCode = statusCode
-    this.isOperational = true // for future
-  }
+class ApiErr extends Error{
+    constructor(statuscode,message){
+        super(message)
+        this.statuscode = statuscode
+        this.isOperationa =  true //it just flag we see in future
+        Error.captureStackTrace(this,this.constructor)
+    }
+
+    static badRequest(message="Bad Request"){
+        return new ApiErr(400,message)
+    }
+     static unauthorized(message="Unauthorized"){
+        return new ApiErr(401,message)
+    }
 }
+
+throw new ApiErr.badRequest
